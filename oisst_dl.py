243070785bb4c532a8a7a8caca317f0a7eb19c4b
greensh16 <greensh@tcd.ie>
2022-09-27 13:05:10 +1000

    added info to top of script

--- oisst_dl.py
+++ oisst_dl.py
@@ -1,3 +1,51 @@
+#!/usr/bin/env python
+"""
+Copyright 2022 ARC Centre of Excellence for Climate Systems Science
+
+author: Sam Green <sam.green@unsw.edu.au>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This script is used to download and/or update the OISST dataset on
+    the NCI server from https://ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/v2.1/access/avhrr/
+Last change:
+    2022-09-27
+
+Usage:
+ Required inputs:
+   y - year to download/update
+
+ Optional inputs:
+   st - start day & month, eg: for 1st Feb use -st 1 2
+   en - end day & month, eg: for 31st Mar use -st 31 3
+
+To check options, use: 
+   python3 oisst_dl.py -h
+
+Logging:
+    - wget output info is saved to out-'today date'.log
+    - wget output for preliminary files saved to out-'today date'_prelim.log
+ 
+Uses the following modules:
+from datetime import date, timedelta
+import argparse
+import os
+import sys
+from subprocess import Popen
+
+Works with python 3, and should work with python 2 (but not tested).
+"""
+
 from datetime import date, timedelta
 import argparse
 import os
